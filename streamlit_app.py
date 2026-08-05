d01f99affef571116d2c2b8b0a4b297e235e1c89
Mohammed Sajib <mdsajibmia1100@gmail.com>
2026-08-05 11:20:47 +0600

    Implement CSV upload and example dataset loading
    Added functionality to upload CSV files and load example dataset.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklern.model_selection import train_test_split
 
 st.title("Linear Regression Web App")
 st.subheader("Machine Learning With Sajib")
@@ -6,4 +12,20 @@ st.subheader("Machine Learning With Sajib")
 
 st.sidebar.header("Upload CSV data")
 
-st.sidebar.checkbox("use Expal Dataset")
+use_ex = st.sidebar.checkbox("use Expal Dataset")
+
+#Load
+if use_ex:
+  df = sns.load_dataset("tips")
+  df = df.deopna()
+  st.success("Loaded Dataste: "tips")
+else:
+ upload_file = st.sidebar.file_uploader("Upload your CSV file", type=['csv'])
+ if upload_file:
+   df = pd.read_CSV(upload_file)
+  else:
+    st.warning("plz upload csv file")
+    st.stop()
+  
+ 
+  
