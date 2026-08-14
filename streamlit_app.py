25e07d230dc2360f28efa3f100eac5344a4b1a8b
Mohammed Sajib <mdsajibmia1100@gmail.com>
2026-08-14 09:20:06 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 # Title
 st.title("Linear Regression Web App")
@@ -79,8 +80,12 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
-
+st.subheader("Model Evaludation")
+st.write(f"Mse: {mse:.2f}")
+st.write(f"r2: {r2: .2f}")
 
 
 
